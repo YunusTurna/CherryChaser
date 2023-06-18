6a470f6b9cefc62b1d6da109b2f6646034d69eb4
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-06-18 19:28:29 +0300

    Cherry Bomb Map taslak oluşturuldu.

--- My project/Assets/Scripts/Player/Pumpkin/PumpkinAnimation.cs
+++ My project/Assets/Scripts/Player/Pumpkin/PumpkinAnimation.cs
@@ -4,5 +4,59 @@ using UnityEngine;
 
 public class PumpkinAnimation : MonoBehaviour
 {
-    
+    Rigidbody rb;
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Walk();
+        Jump();
+        Run();
+        Debug.Log(CharacterMovement.grounded);
+
+
+    }
+    void Walk()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            anim.SetBool("Walk", true);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+
+
+    }
+    void Jump()
+    {
+        if (rb.velocity.y != 0 & CharacterMovement.grounded != true)
+        {
+            anim.SetBool("Jump", true);
+        }
+        else
+        {
+            anim.SetBool("Jump", false);
+        }
+
+    }
+    void Run()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) & (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0))
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+
 }
