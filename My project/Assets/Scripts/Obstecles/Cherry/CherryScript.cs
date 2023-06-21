884751b42929a2209a8d2a951aa31133c81cf8cd
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-06-22 02:56:25 +0300

    Bomb throw fundemantals

--- My project/Assets/Scripts/Obstecles/Cherry/CherryScript.cs
+++ My project/Assets/Scripts/Obstecles/Cherry/CherryScript.cs
@@ -4,39 +4,70 @@ using UnityEngine;
 
 public class CherryScript : MonoBehaviour
 {
-    [SerializeField] private int throwPower = 100;
+    [SerializeField] private int throwPower = 2000;
+    [SerializeField] private int comeBackSpeed = 100;
+    
+    public bool comeBackMethod = false;
+    [SerializeField]private GameObject Parent;
 
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     private void Update()
     {
+        if(comeBackMethod == true)
+        {
+            ComeBackMethod();
+            
+
+        }
         if (transform.parent != null && Input.GetKeyDown(KeyCode.E))
         {
             transform.parent = null;
             rb.isKinematic = false;
-            rb.AddForce(transform.forward * throwPower * Time.deltaTime, ForceMode.Impulse);
+            rb.AddForce((transform.forward + transform.up) * throwPower * Time.deltaTime, ForceMode.Impulse);
+            
         }
+        if(Vector3.Distance(transform.position , Parent.transform.position) > 5f)
+        {
+            this.gameObject.GetComponent<SphereCollider>().enabled = true;
+        }
+        
+        
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameObject parent = other.gameObject;
-            transform.parent = parent.transform;
-            transform.localPosition = new Vector3(0, 10, 0);
+            
+            comeBackMethod = false;
+            Parent = other.gameObject;
+            transform.parent = Parent.transform;
+            transform.localPosition = new Vector3(0, 8, 0);
             transform.localRotation = Quaternion.identity;
             rb.isKinematic = true;
         }
-        else if (other.gameObject.CompareTag("Ground"))
+        if (other.gameObject.CompareTag("Ground"))
         {
+            comeBackMethod = true;
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
+            rb.isKinematic = true;
         }
+        
+        
+    }
+    
+    private void ComeBackMethod()
+    {
+        
+        
+        transform.position = Vector3.MoveTowards(transform.position , Parent.transform.position , comeBackSpeed * Time.deltaTime);
     }
 }
