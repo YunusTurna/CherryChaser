fd2751935eb144c9ad854a3881f72e6d5610f6c9
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-06-22 15:16:05 +0300

    Explosion mechanic fundemantals

---
+++ My project/Assets/Scripts/Obstecles/Cherry/Explosion.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] GameObject Cherry;
+    [SerializeField] private Timer timer;
+
+    public bool explosion = true;
+
+    private void Awake()
+    {
+        this.gameObject.GetComponent<SphereCollider>().enabled = false;
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+    void Start()
+    {
+        timer = GameObject.Find("ExplosionTimer").GetComponent<Timer>();
+
+    }
+
+
+    void Update()
+    {
+        if (timer.countDown == 0 & explosion == true)
+        {
+            transform.position = Cherry.transform.position;
+            explosion = false;
+            this.gameObject.GetComponent<SphereCollider>().enabled = true;
+            this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+
+        }
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        Destroy(other.gameObject);
+
+    }
+}
---
+++ My project/Assets/Scripts/Obstecles/Timer/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    public float countDown;
+
+    private float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 1f)
+        {
+            timer -= 1f;
+            countDown--;
+
+            if (countDown <= 0)
+            {
+                countDown = 0;
+                timerText.text = "BOOM";
+                // İstediğiniz başka bir şeyi yapabilirsiniz
+            }
+        }
+        timerText.text = countDown.ToString();
+        if (countDown == 0)
+        {
+            timerText.text = "BOOM";
+        }
+
+
+    }
+}
