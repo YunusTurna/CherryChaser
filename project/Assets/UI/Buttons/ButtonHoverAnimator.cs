9502491ad92c6222430ff963f4aadc41c9c2724a
fecitekme <arifkaraca@protonmail.com>
2023-07-14 21:31:34 +0300

    ana menü ui

---
+++ project/Assets/UI/Buttons/ButtonHoverAnimator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHoverAnimator : MonoBehaviour
+{
+    public Animator animator;
+
+    void OnMouseEnter()
+    {
+            animator.enabled = true;
+            animator.Play("move");
+    }
+
+    void OnMouseExit()
+    {
+        animator.enabled = true;
+        animator.Play("sidekick");
+    }
+}
